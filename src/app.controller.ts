15afe95686258f674afcc525b5d43cccfe84c34d
rebelliousi <learneryolo3@gmail.com>
2026-08-06 19:17:38 +0500

    feat:create new method getprofile and trying to get interceptor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Profile {
+  username: string;
+  email: string;
+  secretFeature: string;
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +13,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('/profile')
+  getProfile(): Profile {
+    return {
+      username: 'rebel',
+      email: 'rebal@gmail.com',
+      secretFeature: 'this is in the test for now',
+    };
   }
 }
